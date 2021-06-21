21652d8ff8fc532d8144ba5a10a743162c761a05
mertcandav <mertcandav@outlook.com>
2021-06-21 19:59:36 +0300

    Add new example algorithms.

--- scripts/example_algorithms.js
+++ scripts/example_algorithms.js
@@ -1,55 +1,71 @@
-const code_helloworld = `print("Hello 世界!")`
+const code_helloworld = `print('Hello, 世界!')`
 const code_fibonacci =
-`func fib(n)
-  var (
-    count = 0
-    a     = 0
-    b     = 1
-  )
-  for count < n
-    print(a, fin=" ")
-    const c = a + b
-    a = b
-    b = c
-    count += 1
-  end
+`func fib(a, b)
+    var val = a + b
+    print(val)
+    if val < 1000
+        fib(b, val)
+    end
 end
 
-fib(15)
+fib(0, 1)
 `
 const code_primecheck =
-`for _, x in range(2, 100)
-  if x == 2
-    print(x)
-    continue
-  elif x < 2 || x % 2 == 0
-    continue
-  end
-
-  var prime = true
-  for _, y in range(2, x - 1)
-    if x % y == 0
-      prime = false
-      break
+`func prime(x)
+    if x == 2
+        ret true
+    elif x < 2 || x % 2 == 0
+        ret false
     end
-  end
-  if prime
-    print(x)
-  end
+    for _, y in range(2, x - 1)
+        if x % y == 0
+            ret false
+        end
+    end
+    ret true
+end
+
+for _, i in range(2, 100)
+    if prime(i)
+        print(i)
+    end
 end
 `
 const code_fizzbuzz =
 `for _, number in range(1, 100)
-  if number % 3 == 0 && number % 5 == 0
-    print("FizzBuzz")
-  elif number % 3 == 0
-    print("Fizz")
-  elif number % 5 == 0
-    print("Buzz")
-  else
-    print(number)
-  end
+    if number % 3 == 0 && number % 5 == 0
+        print('FizzBuzz')
+    elif number % 3 == 0
+        print('Fizz')
+    elif number % 5 == 0
+        print('Buzz')
+    else
+        print(number)
+    end
 end
+`
+const code_factorial =
+`func fact(x)
+    if x < 0
+        ret NaN
+    elif x == 0
+        ret 1
+    end
+    ret x * fact(x - 1)
+end
+
+print(fact(12))
+`
+const code_hellocountries =
+`func hello(...countries)
+    print('Hello', fin='')
+    for _, c in countries
+        print(', ', fin=c)
+    end
+    print('!')
+end
+
+hello('Turkey', 'France', 'German', 'Japan', 'Canada')
 `
 
 const codeblock = document.getElementById('algorithm_example_block')
@@ -57,17 +73,25 @@ const codeblock = document.getElementById('algorithm_example_block')
 codeblock.textContent = code_helloworld
 
 document.getElementById('example_helloworld').addEventListener('click', function() {
-  codeblock.textContent = code_helloworld
+	codeblock.textContent = code_helloworld
 })
 
 document.getElementById('example_fibonacci').addEventListener('click', function() {
-  codeblock.textContent = code_fibonacci
+	codeblock.textContent = code_fibonacci
 })
 
 document.getElementById('example_primecheck').addEventListener('click', function() {
-  codeblock.textContent = code_primecheck
+	codeblock.textContent = code_primecheck
 })
 
 document.getElementById('example_fizzbuzz').addEventListener('click', function() {
-  codeblock.textContent = code_fizzbuzz
+	codeblock.textContent = code_fizzbuzz
+})
+
+document.getElementById('example_factorial').addEventListener('click', function() {
+	codeblock.textContent = code_factorial
+})
+
+document.getElementById('example_hellocountries').addEventListener('click', function() {
+	codeblock.textContent = code_hellocountries
 })
