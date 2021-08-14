b2d45f8ff67c922d40b33fbcc9700cbc3e1aa7ea
mertcandav <mertcandav@outlook.com>
2021-08-14 19:20:40 +0300

    Update syntax.

--- scripts/example_algorithms.js
+++ scripts/example_algorithms.js
@@ -5,11 +5,11 @@ const code_fibonacci =
 `package main
 
 func fib(a, b) {
-    val = a + b
-    println(val)
-    if val < 1000 {
-        fib(b, val)
-    }
+  val = a + b
+  println(val)
+  if val < 1000 {
+    fib(b, val)
+  }
 }
 
 fib(0, 1)`
@@ -17,48 +17,48 @@ const code_primecheck =
 `package main
 
 func prime(x) {
-    if x == 2 {
-        return true
-    } else if x < 2 || x % 2 == 0 {
-        return false
-    }
-    for _, y in range(2, x - 1) {
-        if x % y == 0 {
-            return false
-        }
-    }
+  if x == 2 {
     return true
+  } else if x < 2 || x % 2 == 0 {
+    return false
+  }
+  for _, y in range(2, x - 1) {
+    if x % y == 0 {
+      return false
+    }
+  }
+  return true
 }
 
 for _, i in range(2, 100) {
-    if prime(i) {
-        println(i)
-    }
+  if prime(i) {
+    println(i)
+  }
 }`
 const code_fizzbuzz =
 `package main
 
 for _, number in range(1, 100) {
-    if number % 3 == 0 && number % 5 == 0 {
-        println('FizzBuzz')
-    } else if number % 3 == 0 {
-        println('Fizz')
-    } else if number % 5 == 0 {
-        println('Buzz')
-    } else {
-        println(number)
-    }
+  if number % 3 == 0 && number % 5 == 0 {
+    println('FizzBuzz')
+  } else if number % 3 == 0 {
+    println('Fizz')
+  } else if number % 5 == 0 {
+    println('Buzz')
+  } else {
+    println(number)
+  }
 }`
 const code_factorial =
 `package main
 
 func fact(x) {
-    if x < 0 {
-        return nan
-    } else if x == 0 {
-        return 1
-    }
-    return x * fact(x - 1)
+  if x < 0 {
+    return nan
+  } else if x == 0 {
+    return 1
+  }
+  return x * fact(x - 1)
 }
 
 println(fact(12))`
@@ -66,11 +66,11 @@ const code_hellocountries =
 `package main
 
 func hello(...countries) {
-    print('Hello')
-    for _, c in countries {
-        print(', ', c)
-    }
-    println('!')
+  print('Hello')
+  for _, c in countries {
+    print(', ', c)
+  }
+  println('!')
 }
 
 hello('Turkey', 'France', 'German', 'Japan', 'Canada')`
@@ -80,25 +80,25 @@ const codeblock = document.getElementById('algorithm_example_block')
 codeblock.textContent = code_helloworld
 
 document.getElementById('example_helloworld').addEventListener('click', function() {
-	codeblock.textContent = code_helloworld
+  codeblock.textContent = code_helloworld
 })
 
 document.getElementById('example_fibonacci').addEventListener('click', function() {
-	codeblock.textContent = code_fibonacci
+  codeblock.textContent = code_fibonacci
 })
 
 document.getElementById('example_primecheck').addEventListener('click', function() {
-	codeblock.textContent = code_primecheck
+  codeblock.textContent = code_primecheck
 })
 
 document.getElementById('example_fizzbuzz').addEventListener('click', function() {
-	codeblock.textContent = code_fizzbuzz
+  codeblock.textContent = code_fizzbuzz
 })
 
 document.getElementById('example_factorial').addEventListener('click', function() {
-	codeblock.textContent = code_factorial
+  codeblock.textContent = code_factorial
 })
 
 document.getElementById('example_hellocountries').addEventListener('click', function() {
-	codeblock.textContent = code_hellocountries
+  codeblock.textContent = code_hellocountries
 })
